d90585db3fe2d3638379522a7655c836ad390350
GengRuotong <ruotonggeng@bupt.edu.cn>
2022-10-17 20:43:40 +0800

    update

--- bash_test.py
+++ bash_test.py
@@ -1,5 +1,5 @@
 import os
-import shutil
+# python -m torch.distributed.launch --nproc_per_node 2 --master_port 1234 train.py \
 
 def mkdir(folder_path):
     # 功能：如果文件夹不存在则创建文件夹，否则删掉原有文件夹创建新文件夹
@@ -8,31 +8,37 @@ def mkdir(folder_path):
     if not isExists:
         os.makedirs(folder_path)
     else:
-        shutil.rmtree(folder_path)
-        os.makedirs(folder_path)
 
-domain_list = ['mt_maoyanyanchu', 'mt_taxi-yonghu', 'mt_maicai', 'mt_waimai', 'mt_youxuan', 'mt_multi']
-output_folder = "output/T5_base_prefix_summary/"
+        print("Folder exists for %s" %(folder_path))
+
+domain_list = ['mt_maoyanyanchu', 'mt_taxi-yonghu', 'mt_maicai', 'mt_waimai', 'mt_youxuan']
+input_folder = "data/sample_datas_wo_prefix/single_domain/"
+# ft
+output_folder = "output/T5_base_ft_wo_prefix/multi_train_single_test/"
+# prefix-tuning
+# output_folder = "output/T5_base_prefix_summary/multi_train_single_test/"
 
 for domain_name in domain_list:
+    input_path = input_folder + domain_name + '/'
     output_path = output_folder + domain_name
     mkdir(output_path)
 
     os.system("""
 export WANDB_API_KEY=3b9858e8352beadda80313599d455c2abfde4ba7
-export WANDB_PROJECT=T5_base_prefix_tuning
-export WANDB_ENTITY=mt_prefix_tuning
+export WANDB_PROJECT=T5_base_finetune
+export WANDB_ENTITY=ruotonggeng
 
-CUDA_VISIBLE_DEVICES=1 python try.py \
-    --run_name T5_base_prefix_summary \
-    --seed 2 \
-    --cfg Salesforce/T5_base_prefix_summary.cfg \
-    --do_train \
-    --do_eval \
-    --do_predict \
+CUDA_VISIBLE_DEVICES=1 python train.py \
+    --run_name T5_base_finetune_summary \
+    --pretrained_model_path pretrained_model/chinese_t5_pegasus_base/ \
     --domain_name %s \
-    --predict_with_generate \
-    --num_train_epochs 5 \
+    --data_folder_path %s \
+    --output_dir %s \
+    --seed 2 \
+    --cfg Salesforce/T5_base_finetune_summary.cfg \
+    --load_weights_from output/T5_base_ft_wo_prefix/multi_domain/checkpoint-15000/pytorch_model.bin \
+    --do_predict \
+    --num_train_epochs 8 \
     --gradient_accumulation_steps 4 \
     --logging_strategy steps \
     --logging_first_step true \
@@ -46,9 +52,8 @@ CUDA_VISIBLE_DEVICES=1 python try.py \
     --save_total_limit 1 \
     --load_best_model_at_end \
     --adafactor true \
-    --learning_rate 1e-3 \
+    --learning_rate 1e-4 \
     --predict_with_generate \
-    --output_dir %s \
     --overwrite_output_dir \
     --per_device_train_batch_size 2 \
     --per_device_eval_batch_size 8 \
@@ -56,4 +61,4 @@ CUDA_VISIBLE_DEVICES=1 python try.py \
     --generation_max_length 128 \
     --input_max_length 512 \
     --num_beams=1 
-""" %(domain_name, output_path))
+""" %(domain_name, input_path, output_path))
--- bash_train.py
+++ bash_train.py
@@ -1,38 +1,41 @@
 import os
-import shutil
 
 def mkdir(folder_path):
-    # 功能：如果文件夹不存在则创建文件夹，否则删掉原有文件夹创建新文件夹
+    # 功能：如果文件夹不存在则创建文件夹，否则报告文件夹路径
     folder_path = folder_path.strip().rstrip("//")
     isExists = os.path.exists(folder_path)
     if not isExists:
         os.makedirs(folder_path)
     else:
-        shutil.rmtree(folder_path)
-        os.makedirs(folder_path)
+        print("Folder exists for %s" %(folder_path))
 
-domain_list = ['mt_maoyanyanchu', 'mt_taxi-yonghu', 'mt_maicai', 'mt_waimai', 'mt_youxuan']
-output_folder = "output/T5_base_prefix_summary/"
+domain_list = ['mt_maoyanyanchu', 'mt_taxi-yonghu', 'mt_maicai', 'mt_waimai', 'mt_youxuan', 'mt_multi']
+input_folder = "data/sample_datas_w_prefix/"
+output_folder = "output/T5_base_finetune_w_prefix/"
 
 for domain_name in domain_list:
+    input_path = input_folder + domain_name + '/'
     output_path = output_folder + domain_name
     mkdir(output_path)
 
     os.system("""
 export WANDB_API_KEY=3b9858e8352beadda80313599d455c2abfde4ba7
-export WANDB_PROJECT=T5_base_prefix_tuning
-export WANDB_ENTITY=mt_prefix_tuning
+export WANDB_PROJECT=T5_base_finetune_w_prefix
+export WANDB_ENTITY=ruotonggeng
 
-CUDA_VISIBLE_DEVICES=0 python train.py \
-    --run_name T5_base_prefix_summary \
+CUDA_VISIBLE_DEVICES=0, 1, 2, 3 python train.py \
+    --run_name T5_base_finetune_w_prefix \
+    --pretrained_model_path pretrained_model/chinese_t5_pegasus_base/ \
+    --domain_name %s \
+    --data_folder_path %s \
+    --output_dir %s \
     --seed 2 \
-    --cfg Salesforce/T5_base_prefix_summary.cfg \
+    --cfg Salesforce/T5_base_finetune_summary.cfg \
     --do_train \
     --do_eval \
     --do_predict \
-    --domain_name %s \
     --predict_with_generate \
-    --num_train_epochs 25 \
+    --num_train_epochs 5 \
     --gradient_accumulation_steps 1 \
     --logging_strategy steps \
     --logging_first_step true \
@@ -48,12 +51,11 @@ CUDA_VISIBLE_DEVICES=0 python train.py \
     --adafactor true \
     --learning_rate 1e-4 \
     --predict_with_generate \
-    --output_dir %s \
     --overwrite_output_dir \
-    --per_device_train_batch_size 16 \
-    --per_device_eval_batch_size 32 \
+    --per_device_train_batch_size 4 \
+    --per_device_eval_batch_size 8 \
     --generation_num_beams 1 \
     --generation_max_length 128 \
     --input_max_length 512 \
     --num_beams=1 
-""" %(domain_name, output_path))
+""" %(domain_name, input_path, output_path))
